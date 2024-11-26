584b666c0b1fa3d5f4eef5b83ae0a699b82c140c
unknown <quynhdinh704@gmail.com>
2024-11-26 20:22:06 +0700

    ..

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,10 +2,7 @@ package test;
 
 import gui.LoginGUI;
 import API.API_Server;
-import controllers.CTPhieuNhapController;
-import dto.CTPhieuNhapDTO;
 import java.io.IOException;
-import java.util.List;
 public class Test {
     public static void main(String[] args) {
         new Thread(() -> {
@@ -16,14 +13,6 @@ public class Test {
             }
         }).start();
         new LoginGUI();
-        
-        CTPhieuNhapController controller = new CTPhieuNhapController();
-        List<CTPhieuNhapDTO> ctpnList = controller.getCTPhieuNhapList();
-        if(ctpnList != null) {
-            for(CTPhieuNhapDTO ct : ctpnList) {
-                System.out.println(ct.getIdPhieuNhap());
-            }
-        }
 
     }
 }
